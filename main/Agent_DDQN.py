8f79a3e177a65d3caa765ecb42b4bfe86bb76a43
xiongli <18811179838@163.com>
2021-12-30 20:57:05 +0800

    DDQN训练函数

--- main/Agent_DDQN.py
+++ main/Agent_DDQN.py
@@ -47,9 +47,6 @@ class ReplayBuffer:
     def size(self):
         return len(self.buffer)
 
-    def size(self):
-        return len(self.buffer)
-
 
 class Qnet(keras.Model):
     def __init__(self, action_size):
@@ -79,3 +76,32 @@ class Qnet(keras.Model):
             return random.randint(0, self.action_size-1)
         else:  # 选择Q值最大的动作
             return int(tf.argmax(out))
+
+def train(q, q_target, memory, optimizer, batch_size, gamma):
+    for i in range(10):
+        # 从缓冲池采样
+        s, a, r, s_prime, done_mask = memory.sample(batch_size)
+        with tf.GradientTape() as tape:
+            # s: [b, 60]
+            q_out = q(s)  # 得到Q(s,a)的分布
+            # 由于TF的gather_nd与pytorch的gather功能不一样，需要构造
+            # gather_nd需要的坐标参数，indices:[b, 2]
+            # pi_a = pi.gather(1, a) # pytorch只需要一行即可实现
+            indices = tf.expand_dims(tf.range(a.shape[0]), axis=1)
+            indices = tf.concat([indices, a], axis=1)
+            q_a = tf.gather_nd(q_out, indices)  # 动作的概率值, [b]
+            q_a = tf.expand_dims(q_a, axis=1)  # [b]=> [b,1]
+            # 得到Q(s',a)的最大值，它来自影子网络！ [b,4]=>[b,2]=>[b,1]
+            a_ = tf.expand_dims(tf.argmax(q(s_prime), axis=1), axis=1)
+            a_ = tf.cast(a_, tf.int32)
+            indices_ = tf.expand_dims(tf.range(a.shape[0]), axis=1)
+            indices_ = tf.concat([indices_, a_], axis=1)
+            max_q_prime = tf.gather_nd(q_target(s_prime), indices_)
+            # max_q_prime = tf.reduce_max(q_target(s_prime), axis=1, keepdims=True)
+            # 构造Q(s,a_t)的目标值，来自贝尔曼方程
+            target = r + gamma * max_q_prime
+            # 计算Q(s,a_t)与目标值的误差
+            loss = losses.MSE(target, q_a)
+            # 更新网络，使得Q(s,a_t)估计符合贝尔曼方程
+            grads = tape.gradient(loss, q.trainable_variables)
+            optimizer.apply_gradients(zip(grads, q.trainable_variables))
